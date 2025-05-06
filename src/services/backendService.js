c7f93f249b049cd4f552b50535709d1eab0d2e32
Simon Danielsson <sd223ah.student.lnu.se>
2025-05-06 10:47:58 +0200

    Add backend service for data fetching

---
+++ src/services/backendService.js
@@ -0,0 +1,38 @@
+/**
+ * Returns the lowest available year and highest available year.
+ *
+ * @return {object} - An object containing "highest_year" and "lowest_year".
+ */
+export async function getYearRange() {
+  return await fetchData('/api/stats')
+}
+
+/**
+ * Fetches data based on year.
+ *
+ * @param {number} year - The year. 
+ * @param {undefined} [field=undefined] - Optional field to filter by.
+ */
+export async function getByYear(year, field = undefined) {
+  return await fetchData(`/api/${year}${field ? `/field` : ''}`)
+}
+
+export async function getTopByField(year, field) {
+  return await fetchData(`/api/${year}/${field}/top`)
+}
+
+export async function getSummaryOfField(year, field) {
+  return await fetchData(`/api/${year}/${field}/summary`)
+}
+
+async function fetchData(url) {
+  const res = await fetch(url, {
+    method: 'GET'
+  })
+
+  if (!res.ok) {
+    throw new Error(`Couldn't fetch data stats, server responded with a ${res.status}`)
+  }
+
+  return await res.json()
+}
