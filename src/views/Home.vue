258e32fdfd7d19cba86a400ecf6852c2f6a93c78
Simon Danielsson <sd223ah.student.lnu.se>
2025-05-12 14:58:30 +0200

    Update home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="home-page">
-    <h1>Home</h1>
-    <p>This is the home page</p>
+    <h1>Welcome</h1>
+    <p>All data used is provided by <a href="https://ourworldindata.org/" target="_blank">Our World in Data</a>.</p>
   </main>
 </template>
